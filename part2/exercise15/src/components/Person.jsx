89ecf17703036f40a8fbc6366e387340ae2f4052
westolt <westolt@ad.helsinki.fi>
2025-07-15 18:29:49 +0300

    Remove exercise15

--- part2/exercise15/src/components/Person.jsx
+++
@@ -1,7 +0,0 @@
-const Person = ({ person, removePerson }) => {
-  return <li>
-    {person.name} {person.number} <button onClick={removePerson}>delete</button>
-    </li>
-}
-
-export default Person
