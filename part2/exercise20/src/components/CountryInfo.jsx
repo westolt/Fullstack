f398d090282d4f78210fd4741ac83a2ac78c67ed
westolt <westolt@ad.helsinki.fi>
2025-07-18 22:38:48 +0300

    Exercise 2.20

--- part2/exercise20/src/components/CountryInfo.jsx
+++ part2/exercise20/src/components/CountryInfo.jsx
@@ -1,6 +1,23 @@
+import { useState, useEffect } from "react"
+import axios from 'axios'
 import Languages from "./Languages"
 
 const CountryInfo = ({ country }) => {
+    const [weather, setWeather] = useState(null)
+
+    useEffect(() => {
+        console.log('effect run, city is now', country.capital)
+
+        if (country.capital) {
+            const api_key = import.meta.env.VITE_SOME_KEY
+            axios
+            .get(`https://api.openweathermap.org/data/2.5/weather?q=${country.capital}&units=metric&appid=${api_key}`)
+            .then(response => {
+                setWeather(response.data)
+            })
+        }
+    }, [country.capital])
+
     return(
         <div>
             <h1>{country.name.common}</h1>
@@ -11,6 +28,15 @@ const CountryInfo = ({ country }) => {
             <h2>Languages</h2>
             <Languages languages={country.languages} />
             <img className="flag" src={country.flags.png} />
+            {weather ?
+            <>
+            <h2>Weather in {country.capital}</h2>
+            <p>Temperature {weather.main.temp} Celsius</p>
+            <img src={`https://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`} />
+            <p>Wind {weather.wind.speed} m/s</p>
+            </>
+            : <p>No weather data</p>
+            }
         </div>
     )
 }
