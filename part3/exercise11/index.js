b10e991ebfe25e473eded0e7ab75ee85155d48f0
westolt <westolt@ad.helsinki.fi>
2025-07-24 22:50:57 +0300

    Exercises 3.7 and 3.8

--- part3/exercise11/index.js
+++ part3/exercise11/index.js
@@ -1,7 +1,12 @@
 const express = require('express')
-const app = express()
+const morgan = require('morgan')
 
+morgan.token('body', (req) => JSON.stringify(req.body))
+
+const app = express()
 app.use(express.json())
+
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 
 let persons = [
     {
