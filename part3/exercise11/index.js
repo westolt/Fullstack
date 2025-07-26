b9892baf346152137f05b60686bb609b72e1959c
westolt <westolt@ad.helsinki.fi>
2025-07-26 16:46:26 +0300

    Copy frontend dist to backend

--- part3/exercise11/index.js
+++ part3/exercise11/index.js
@@ -1,10 +1,15 @@
 const express = require('express')
 const morgan = require('morgan')
+const cors = require('cors')
 
 morgan.token('body', (req) => JSON.stringify(req.body))
 
 const app = express()
 app.use(express.json())
+
+app.use(cors())
+
+app.use(express.static('dist'))
 
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 
@@ -107,7 +112,7 @@ app.get('/info', (request, response) => {
         `)
 })
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
 })
