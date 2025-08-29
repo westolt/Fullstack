d670f155ed3072e816e75b5685a90b801ee2f480
westolt <westolt@ad.helsinki.fi>
2025-08-29 13:10:29 +0300

    Exercise 5.17

---
+++ part5/E2E-tests/tests/blog_app.spec.js
@@ -0,0 +1,23 @@
+const { test, expect, beforeEach, describe } = require('@playwright/test')
+
+describe('Blog app', () => {
+  beforeEach(async ({ page, request }) => {
+    await request.post('http://localhost:3003/api/testing/reset')
+    await request.post('http://localhost:3003/api/users', {
+      data: {
+        name: 'Testi Teemu',
+        username: 'testi',
+        password: '123'
+      }
+    })
+
+    await page.goto('http://localhost:5173')
+  })
+
+  test('Login form is shown', async ({ page }) => {
+    await expect(page.getByText('Log in to application')).toBeVisible()
+    await expect(page.getByText('username')).toBeVisible()
+    await expect(page.getByText('password')).toBeVisible()
+    await expect(page.getByText('login')).toBeVisible()
+  })
+})
---
+++ test/index.js
@@ -0,0 +1,19 @@
+const express = require('express')
+const app = express()
+
+let notes = [
+
+]
+
+app.get('/', (request, response) => {
+  response.send('<h1>Hello World!</h1>')
+})
+
+app.get('/api/notes', (request, response) => {
+  response.json(notes)
+})
+
+const PORT = 3001
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
