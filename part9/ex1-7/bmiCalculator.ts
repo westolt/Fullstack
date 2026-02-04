492d3b8ce0fa44c1770679259d1025b551689ecf
westolt <william.stolt@helsinki.fi>
2026-02-04 19:40:56 +0200

    Exercise 9.4

--- part9/ex1-7/bmiCalculator.ts
+++ part9/ex1-7/bmiCalculator.ts
@@ -21,20 +21,22 @@ const calculateBmi = (height: number, weight: number): string => {
     const bmi: number = (weight/(height)**2) * 10000
     if (bmi < 16) {
         return 'Severe Thinness'
-    }else if (bmi >= 16 && bmi < 17) {
+    } else if (bmi >= 16 && bmi < 17) {
         return 'Moderate Thinness'
-    }else if (bmi >= 17 && bmi < 18.5) {
+    } else if (bmi >= 17 && bmi < 18.5) {
         return 'Mild Thinness'
-    }else if (bmi >= 18.5 && bmi < 25) {
+    } else if (bmi >= 18.5 && bmi < 25) {
         return 'Normal'
-    }else if (bmi >= 25 && bmi < 30) {
+    } else if (bmi >= 25 && bmi < 30) {
         return 'Overweight'
-    }else if (bmi >= 30 && bmi < 35) {
+    } else if (bmi >= 30 && bmi < 35) {
         return 'Obese Class I'
-    }else if (bmi >= 35 && bmi < 40) {
+    } else if (bmi >= 35 && bmi < 40) {
         return 'Obese Class II'
-    }else if (bmi >= 40) {
+    } else if (bmi >= 40) {
         return 'Obese Class III'
+    } else {
+        throw new Error('Something is no working');
     }
 }
 
