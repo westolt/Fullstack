6415375e686fc488b97ac11e570dfa1402c3e674
westolt <william.stolt@helsinki.fi>
2026-02-05 17:40:36 +0200

    Exercise 9.15

---
+++ part9/ex15-20/my-app-name/src/components/Total.tsx
@@ -0,0 +1,12 @@
+const Total = ({ total }: { total: number }) => {
+  
+    return(
+        <div>
+            <p>
+                Number of exercises {total}
+            </p>
+        </div>
+    )
+}
+
+export default Total
